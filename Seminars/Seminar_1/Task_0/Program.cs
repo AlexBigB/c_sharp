a59e993a2578a79b99622e4f5a2a9d62ef486301
Alexander_Belozerov <a.y.belozerov@gmail.com>
2022-05-06 05:18:53 +0300

    Seminars added_ task0

---
+++ Seminars/Seminar_1/Task_0/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine());
+int numResult = num * num;
+Console.Write("Квадрат числа равен: ");
+Console.WriteLine(numResult);
