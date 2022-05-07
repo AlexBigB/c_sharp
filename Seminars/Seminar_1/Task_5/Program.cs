7a51b0fd06d10bc09e2402a331b63521ecdbdaa8
Alexander_Belozerov <a.y.belozerov@gmail.com>
2022-05-07 16:13:32 +0300

    добавил task 5

---
+++ Seminars/Seminar_1/Task_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+num = Math.Abs(num);
+int counter = 0-num;
+while (counter < num + 1)
+{
+    Console.Write(counter);
+    Console.Write(", ");
+    counter ++;
+}
+
+
