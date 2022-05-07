78f34e486bac6a4ac3bf5d07d31bd34680abad51
Alexander_Belozerov <a.y.belozerov@gmail.com>
2022-05-07 14:27:34 +0300

    исправил task 001

--- Seminars/Seminar_1/Task_0/Program.cs
+++ Seminars/Seminar_1/Task_0/Program.cs
@@ -1,6 +1,7 @@
-﻿Console.Write("Введите целое число: ");
+﻿Console.Clear();
+Console.Write("Введите целое число: ");
 int num = int.Parse(Console.ReadLine());
 int numResult = num * num;
 Console.Write("Квадрат числа равен: ");
 Console.WriteLine(numResult);
-Console.ReadKey();
+//Console.ReadKey();    //нажать кнопку, чтобы выйти
