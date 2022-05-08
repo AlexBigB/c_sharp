2f0a6318c06ba73d23ed7d5bdcd0e974bfb525cd
Alexander_Belozerov <a.y.belozerov@gmail.com>
2022-05-08 09:40:24 +0300

    task 2 added

---
+++ Seminars/Seminar_1/Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход
+принимает два числа и выдаёт, какое число большее, а
+какое меньшее.
+Примеры:
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+Console.Clear();
+Console.Write("First number: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Second number: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a < b) Console.WriteLine(b);
+else Console.WriteLine(a);
--- Seminars/Seminar_1/Task_7/Program.cs
+++ Seminars/Seminar_1/Task_7/Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Введите 3х значное число: ");
-int num = int.Parse(Console.ReadLine());
+//int num = int.Parse(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(num % 10);
