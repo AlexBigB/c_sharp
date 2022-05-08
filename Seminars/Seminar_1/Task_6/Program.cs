137f8fb72c3a48b16763daf707568b0fe023a608
Alexander_Belozerov <a.y.belozerov@gmail.com>
2022-05-08 09:58:06 +0300

    task 6 added

---
+++ Seminars/Seminar_1/Task_6/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 6: Напишите программу, которая на вход
+принимает число и выдаёт, является ли число чётным
+(делится ли оно на два без остатка).
+Примеры:
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Clear();
+Console.Write("Enter your number: ");
+int a = int.Parse(Console.ReadLine());
+bool res = a%2==0;
+Console.WriteLine(res ? "It's an even number" : " It's not an even number");
