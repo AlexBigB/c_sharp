e0dd0778b660a6cec0c53f9b7f90464af4dd9bfa
Alexander_Belozerov <a.y.belozerov@gmail.com>
2022-05-19 20:19:51 +0300

    task 15 added

---
+++ Seminars/Seminar_1/Task_10/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите числа через пробел: ");
+string s = Console.ReadLine();
+Console.WriteLine(s);
+string[] str_arr=s.Split(' ').ToArray();
+Console.WriteLine(str_arr.Length);
+int[] array = new int[str_arr.Length];
+
+int[] myStrToInt(string[] s_arr,int[] i_arr)
+{   
+    int counter = 0;
+    while (counter < s_arr.Length)
+    {
+        i_arr[counter] = int.Parse(s_arr[counter]);
+        counter++;
+    }
+}
+
+
+
+
+Console.WriteLine(i);
