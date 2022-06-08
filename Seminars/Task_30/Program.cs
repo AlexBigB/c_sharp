08511ff5fba3bfe8b558eec12709e2f78f9d38cc
Alexander Belozerov <a.y.belozerov@gmail.com>
2022-06-08 16:45:26 +0300

    seminar 4 added

--- Seminars/Task_30/Program.cs
+++ Seminars/Task_30/Program.cs
@@ -2,3 +2,19 @@
 // выводит массив из 8 элементов, заполненный
 // нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
+Console.Clear();
+int[] int_arr = new int[8];
+
+void FillArray(int[] col1)
+{
+    Console.Write($"[");
+    for (int i = 0; i < col1.Length; i++)
+    {
+        col1[i] = new Random().Next(0, 2);
+        Console.Write($"{col1[i]} ");
+    }
+    Console.Write($"]");
+}
+
+FillArray(int_arr);
+
