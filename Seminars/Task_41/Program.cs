55dc1164427f8014a27020195ef8542b33c37c9a
Alexander_Belozerov <a.y.belozerov@gmail.com>
2022-06-17 13:07:29 +0300

    homework 6 added

---
+++ Seminars/Task_41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41. Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.WriteLine("Введите элементы масстива через запятую: ");
+int[] myArr = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+void CountPos(int[] arr)
+{
+    int pos = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) pos++;
+    }
+    Console.WriteLine($"Положительных чисел: {pos}");
+}
+CountPos(myArr);
---
+++ Seminars/Task_43/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку
+// пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1  // x = (b1- b2)/(k1- k2)
+// y = k2 * x + b2; // y = k2(y-k1- b1) + b2 // y = (-k2*k1 - k2*b1 +b2)/(k2 -1)
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 
+// -> (-0,5; -0,5)
+
+Console.Clear();
+Console.Write("Введите переменную b1: ");
+float b1 = float.Parse(Console.ReadLine());
+Console.Write("Введите переменную k1: ");
+float k1 = float.Parse(Console.ReadLine());
+Console.Write("Введите переменную b2: ");
+float b2 = float.Parse(Console.ReadLine());
+Console.Write("Введите переменную k2: ");
+float k2 = float.Parse(Console.ReadLine());
+float x = (b1- b2)/(k2- k1);
+float y = k1 * x + b1;
+Console.WriteLine($"Точка пересечения прямых -> ({x}; {y})");
